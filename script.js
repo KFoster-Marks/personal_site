8d9930a5cd45d464a9034d77b59c1553fa9db528
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-04-17 08:24:39 -0700

    Fixes animate, adds smooth scroll

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
   $(document).ready(function() {
     $("h1").addClass("animated pulse");
   });
+
+  $(document).ready(function(){
+  	$('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash,
+  	    $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 700, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
+  });
